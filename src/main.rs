cea33ef9be6fdfcbccb5d74954a31dde4ea27b7c
Ryan Thomas <ryan@ryant.org>
2020-03-07 21:14:40 +1100

    Initial reading of data from the Kraken.
    This adds in a mechanism to read the temp / pump speed / fan speed
    from the device - next step is to ensure we can write the vales back
    to the device.

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+
+pub mod nzxt {
+    const NZXT_VENDOR_ID: u16 = 0x1e71;
+    const NZXT_KRAKEN_X_PRODUCT_ID: u16 = 0x170e;
+
+    pub struct Kraken {
+        device: hidapi::HidDevice,
+    }
+
+    #[derive(Debug)]
+    pub struct KrakenData {
+        liquid_temp: u8,
+        fan_speed: u16,
+        pump_speed: u16,
+        firmware_version: (u8, u16, u8),
+    }
+
+    impl Kraken {
+        pub fn open() -> Result<Kraken, hidapi::HidError> {
+            let api = hidapi::HidApi::new()?;
+            let device = api.open(NZXT_VENDOR_ID, NZXT_KRAKEN_X_PRODUCT_ID)?;
+
+            Ok(Kraken {
+                device,
+            })
+        }
+
+        pub fn read(&self) -> Result<KrakenData, hidapi::HidError> {
+            let mut buf = [0u8; 64];
+            let res = self.device.read_timeout(&mut buf, 1000)?;
+            
+            Ok(KrakenData {
+                liquid_temp: buf[1],
+                fan_speed: (buf[3] as u16) << 8 | buf[4] as u16,
+                pump_speed: (buf[5] as u16) << 8 | buf[6] as u16,
+                firmware_version: (buf[0x0b], (buf[0x0c] as u16) << 8 | buf[0x0d] as u16, buf[0x0e]),
+            })
+        }
+    }
+}
+
+fn main() {
+    let kraken = nzxt::Kraken::open().unwrap();
+    
+    let data = kraken.read().unwrap();
+    
+    println!("DATA: {:?}", data);
+
+    
+
+    
+
+
+}
